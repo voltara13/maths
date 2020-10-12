6f051067aff8839930f2f09d667b29b8a3d9fecf
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-10-13 00:35:25 +0700

    lw2

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
+int A[3][3] = {
+	    {5, 3, 1},
+	    {3, 0, 7},
+	    {1, 8, 1}
+    },
+    B[3] = {12, 10, 18};
+
+
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	
+	for (int i = 0; i < 3; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < 3; j++)
+			cout << A[i][j];
+	}
+
 	return 0;
 }
