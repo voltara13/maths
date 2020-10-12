ed88a14bebfb82ac36c674a39a72b93f3f9cbe50
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-10-13 01:33:25 +0700

    lw2

--- main.cpp
+++ main.cpp
@@ -2,23 +2,56 @@
 
 using namespace std;
 
-int A[3][3] = {
+double A[3][3] = {
 	    {5, 3, 1},
 	    {3, 0, 7},
 	    {1, 8, 1}
     },
-    B[3] = {12, 10, 18};
+    B[3] = {12, 10, 18},
+    A2[3][3] = {
+    	{3, 0, 7},
+    	{5, 3, 1},
+    	{1, 8, 1}
+    },
+	B2[3] = {10, 12, 18},
+    res[3], resN[3];
 
+void SimpleIterations()
+{
+	double eps;
+	cin >> eps;
+	for (int i = 0; i < 3; i++) 
+		res[i] = B2[i] / A2[i][i];
+	do
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			resN[i] = B2[i] / A2[i][i];
+			for (int j = 0; j < 3; j++)
+				if (i != j) 
+					resN[i] -= A2[i][j] / A2[i][i] * res[j];
+		}
+		bool flag = true;
+		for (int i = 0; i < 2; i++)
+		{
+			if (abs(resN[i]) - res[i] > eps)
+			{
+				flag = false;
+				break;
+			}
+		}
+		for (int i = 0; i < 3; i++)
+			res[i] = resN[i];
+		if (flag) break;
+	}
+	while (true);
+}
 
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	for (int i = 0; i < 3; i++)
-	{
-		cout << endl;
-		for (int j = 0; j < 3; j++)
-			cout << A[i][j];
-	}
+	SimpleIterations();
+	for (int i = 0; i < 3; i++) cout << res[i] << " ";
 	return 0;
 }
