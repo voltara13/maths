8439310394497fab066ef54b76bb2ad198803875
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-11-06 16:56:17 +0700

    lw4

--- main.cpp
+++ main.cpp
@@ -115,110 +115,15 @@ void PolynomialInLagrange()
 	cout << "Полином, полученный методом Лагранжа\n" << resStr.str() << endl;
 }
 
-void PolynomialInNewton()
+void CubicSpline()
 {
-	vector<double> res(n);
-	stringstream resStr;
-
-	for (int i = 0; i < n; i++)
-	{
-		double val = 0;
-		for (int j = 0; j <= i; j++)
-		{
-			double seq = 1;
-			for (int k = 0; k <= i; k++)
-				if (k != j) seq *= points[j].first - points[k].first;
-			val += points[j].second / seq;
-		}
-		res[i] = val;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		if (res[i])
-		{
-			if (i)
-			{
-				if (res[i] > 0)
-				{
-					if (resStr.str()[0]) resStr << "+";
-					resStr << res[i];
-				}		
-				else if (res[i] < 0) resStr << res[i];
-				for (int j = 0; j < i; j++)
-				{
-					if (points[j].first)
-					{
-						resStr << "(x";
-						if (points[j].first > 0) resStr << "-" << points[j].first << ")";
-						else if (points[j].first < 0) resStr << "+" << -points[j].first << ")";
-					}
-					else resStr << "x";
-				}
-			}
-			else resStr << res[0];
-		}
-	}
-	cout << "Полином, полученный методом Ньютона\n" << resStr.str() << endl;
-}
-
-void LeastSquareMethod()
-{
-	double max = 0, factor = 0;
-	int iM = 0, jM = 0, polyDeg;
-	cout << "Введите степень многочлена: ";
-	polyDeg = CheckValue();
-	vector<vector<double>> rssA(polyDeg + 1, vector<double>(polyDeg + 1));
-	vector<double> rssB(polyDeg + 1);
-	vector<double> res(polyDeg + 1);
-
-	for (int i = 0; i < polyDeg + 1; i++)
-	{
-		for (auto &it : points)
-			rssB[i] += it.second * pow(it.first, i);
-		for (int j = 0; j < polyDeg + 1; j++)
-		{
-			for (auto &it : points)
-				rssA[i][j] += pow(it.first, i + j);
-			if (abs(rssA[i][j]) > max)
-				max = rssA[i][j], iM = i, jM = j;
-		}
-	}
-	for (int j = 0; j < polyDeg + 1; j++)
-		swap(rssA[0][j], rssA[iM][j]);
-	for (int i = 0; i < polyDeg + 1; i++)
-		swap(rssA[i][0], rssA[i][jM]);
-	swap(rssB[0], rssB[iM]);
-	for (int i = 0; i < polyDeg + 1; i++)
-	{
-		factor = rssA[i][i];
-		for (int j = 0; j < polyDeg + 1; j++)
-			rssA[i][j] /= factor;
-		rssB[i] /= factor;
-		for (int k = i + 1; k < polyDeg + 1; k++)
-			if (rssA[k][i])
-			{
-				factor = rssA[k][i] / rssA[i][i];
-				for (int j = 0; j < polyDeg + 1; j++)
-					rssA[k][j] -= factor * rssA[i][j];
-				rssB[k] -= rssB[i] * factor;
-			}
-	}
-	for (int i = polyDeg; i >= 0; i--)
-	{
-		res[i] = rssB[i] / rssA[i][i];
-		for (int j = 0; j < i; j++)
-			rssB[j] -= rssA[j][i] * res[i];
-	}
-	cout << "Коэффициенты, полученные методом наименьших квадратов\n";
-	for (int i = 0; i < polyDeg + 1; i++) cout << "a" << i << " = " << res[i] << endl;
+	
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	Request();
-	PolynomialInLagrange();
-	PolynomialInNewton();
-	LeastSquareMethod();
+	CubicSpline();
 	return 0;
 }
